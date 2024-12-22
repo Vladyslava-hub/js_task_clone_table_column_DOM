a31de691d9529cccce2abe3c21c24ae5ac86122e
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2024-12-22 17:01:34 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const table = document.querySelector('.dashboard');
+  // eslint-disable-next-line no-unused-vars
+  const row = table.querySelectorAll('tbody tr');
+
+  // eslint-disable-next-line no-undef, no-shadow
+  rows.forEach((row) => {
+    const secondColumn = row.cells[1];
+    const clonedColumn = secondColumn.cloneNode(true);
+
+    row.inserBefore(clonedColumn, row.cells[row.cells.length - 1]);
+  });
+});
